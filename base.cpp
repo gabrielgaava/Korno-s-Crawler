ece4b6143d1f74a194c9685c3b9862bd2fb0c848
heinrichpj <heinrich.zago@unesp.br>
2019-10-12 19:19:15 -0300

    tentativa de ajustar a camera

--- base.cpp
+++ base.cpp
@@ -10,12 +10,13 @@
 //Declara��o de Vari�veis Globais
 int angulo1 = 0, angulo2 = 0, angulo3 = 0;
 int projecao=0; //Vari�vel L�gica para Definir o Tipo de Proje��o (Perspectiva ou Ortogonal)
-int posx=0, posy=10, posz=20; //Vari�veis que definem a posi��o da c�mera
-int oy=0,ox=0,oz=0;         //Vari�veis que definem para onde a c�mera olha
-int lx=0, ly=1,  lz=0;         //Vari�veis que definem qual eixo estar� na vertical do monitor.
+int posx, posy, posz; //Vari�veis que definem a posi��o da c�mera
+int oy,ox,oz;         //Vari�veis que definem para onde a c�mera olha
+int lx, ly,lz;         //Vari�veis que definem qual eixo estar� na vertical do monitor.
 float charx = 0, chary = 0, charz = 0;
 double t = 0;
 float increm = 0.017453293;
+int tipoCam = 1;
 
 //Prot�tipos das Fun��es
 void Inicializa();
@@ -69,6 +70,41 @@ void moveCamera(){
 
 }
 
+void adjustCamera(){
+   if(tipoCam > 0){ //camera 2d
+
+      posx = charx;
+      posy = 30;
+      posz = charz;
+
+      ly = 0;
+      lx = charx;
+      lz = charz;
+
+      oy = 0;
+      ox = 1;
+      oz = 0;
+   }
+   else if(tipoCam < 0){
+
+      posx = charx;
+      posy = chary +2;
+      posz = charz;
+
+      oy = 1;
+      ox = 0;
+      oz = 0;
+
+      ly = 5;
+      lx = charx;
+      lz = charz;
+
+   }
+
+
+}
+
+
 void Display()
 {
    
@@ -97,6 +133,7 @@ void Display()
    glMatrixMode(GL_MODELVIEW);/*glMatrixMode()- define qual matriz ser� alterada. SEMPRE defina a c�mera 
                               (Ortogonal ou Perspectiva) na matriz MODELVIEW (onde o desenho ocorrer�).*/
    glLoadIdentity(); ////"Limpa" ou "transforma" a matriz em identidade, reduzindo poss�veis erros.
+         adjustCamera();    
 
    gluLookAt(posx,posy,posz,ox,oy,oz,lx,ly,lz); //Define a pos da c�mera, para onde olha e qual eixo est� na vertical.
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); /* "limpa" um buffer particular ou combina��es de buffers, 
@@ -106,9 +143,8 @@ void Display()
    //----------------------------------------------------------------
    //glPushMatrix(); //Salva o estado atual da cena. O que for desenhado ap�s o Push n�o influenciar� o j� representado
       glColor3ub(100, 255, 40);
-    
       createMainChar();
-       
+      
 
 
   // glPopMatrix();  //Retorna ao estado anterior da cena. O que for desenhado ap�s o Push n�o influenciou o j� representado
@@ -155,6 +191,11 @@ void keyboard (unsigned char key, int x, int y)
          t = (t + increm);
       moveCamera();
     }
+    else if(key == 'p'){
+         tipoCam *= -1;
+         adjustCamera();
+    }
+
       glutPostRedisplay();
 }
 
