a91692d81956bfe74f7fcc0e53ba40ad7ffab7db
heinrichpj <heinrich.zago@unesp.br>
2019-10-12 18:47:39 -0300

    personagem

--- base.cpp
+++ base.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <GL/glut.h>
+#include <math.h>
 
 
 
@@ -12,6 +13,9 @@ int projecao=0; //Vari�vel L�gica para Definir o Tipo de Proje��o (Persp
 int posx=0, posy=10, posz=20; //Vari�veis que definem a posi��o da c�mera
 int oy=0,ox=0,oz=0;         //Vari�veis que definem para onde a c�mera olha
 int lx=0, ly=1,  lz=0;         //Vari�veis que definem qual eixo estar� na vertical do monitor.
+float charx = 0, chary = 0, charz = 0;
+double t = 0;
+float increm = 0.017453293;
 
 //Prot�tipos das Fun��es
 void Inicializa();
@@ -19,6 +23,52 @@ void Display();
 void Mouse(int botao, int estado, int x, int y);
 void keyboard (unsigned char key, int x, int y);
 void TeclasEspeciais (int key, int x, int y);
+
+
+void createMainChar(){
+     
+      int armsize =4.7,bodysize = 6,headsize = 1;
+      float armposx = 1.7,armposy = 0.5,armposz = 0,headposx = 0,headposy = 5,headposz = 0;
+      float bodyposx = 0,bodyposy = 0,bodyposz = 0;
+
+      glTranslatef(charz,chary,charx);
+
+      glPushMatrix();
+      glColor3f(1.5,0,0);
+      glScalef(1, bodysize, 1);
+      glutWireCube(1.5);
+      glTranslatef(bodyposx, bodyposy, bodyposz);
+      glPopMatrix();
+
+      glPushMatrix();
+      glTranslatef(0.0, 5, 0);
+      glutSolidSphere(1, 50, 50);
+      glPopMatrix();
+
+      glPushMatrix();
+      glScalef(0.6,armsize,0.6);
+      glTranslatef(armposx,armposy,armposz);
+      glRotatef(angulo1,1,0,0);
+      glutWireCube(1);
+      glPopMatrix();
+
+      glPushMatrix();
+      glScalef(0.6,armsize,0.6);
+      glTranslatef(-armposx,armposy,armposz);
+      glRotatef(angulo1,1,0,0);
+      glutWireCube(1);
+      glPopMatrix();
+
+}
+
+void moveCamera(){
+   
+   
+   posx = 30*sin(t);
+   posz = 30*cos(t);
+
+
+}
 
 void Display()
 {
@@ -57,31 +107,8 @@ void Display()
    //----------------------------------------------------------------
    //glPushMatrix(); //Salva o estado atual da cena. O que for desenhado ap�s o Push n�o influenciar� o j� representado
       glColor3ub(100, 255, 40);
-      glutWireSphere(1.0, 50, 50);
-
-      glRotatef(angulo1, 0, 0, 1);
-      glTranslatef(3.0, 0, 0);
-    glPushMatrix();
-      glScalef(4, 1, 1);
-      glColor3ub(255, 0, 0);
-      glutWireCube(1.5);
-    glPopMatrix();
     
-      glTranslatef(3.0, 0, 0);
-      glColor3ub(100, 255, 40);
-      glutWireSphere(1.0, 50, 50);
-
-      glRotatef(angulo2, 0, 0, 1);
-      glTranslatef(3.0, 0, 0);
-    glPushMatrix();
-      glScalef(4, 1, 1);
-      glColor3ub(255, 0, 0);
-      glutWireCube(1.5);
-    glPopMatrix();
-    
-      glTranslatef(3.0, 0, 0);
-      glColor3ub(100, 255, 40);
-      glutWireSphere(1.0, 50, 50);
+      createMainChar();
        
 
 
@@ -118,13 +145,17 @@ void keyboard (unsigned char key, int x, int y)
 {//Key - recebe o c�digo ASCII da tecla
  //x, y - recebem as posi��es do mouse na tela (permite tratar os dois dispositivos)
     if(key == 'w')
-        angulo1 = (angulo1 - 5)%360; 
-    else if(key == 'q')
-        angulo1 = (angulo1 + 5)%360;
+      charx ++;    
     else if(key == 's')
-        angulo2 = (angulo2 - 5)%360;
+        charx --;
     else if(key == 'a')
-        angulo2 = (angulo2 + 5)%360;
+         charz --;
+    else if(key == 'd')
+         charz ++;
+    else if(key == 'q'){
+         t = (t + increm);
+      moveCamera();
+    }
       glutPostRedisplay();
 }
 
